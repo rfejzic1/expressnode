214ac9d12312a65c7076df1c56b715600bb64dce
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2019-08-12 16:55:46 +0200

    Added favicon

--- app.js
+++ app.js
@@ -3,39 +3,49 @@ const greetings = require('./lib/greetings');
 
 const app = express();
 
-// set up handlebars view engine
+// Disable x-powered-by header in the response object
+app.disable('x-powered-by');
+
+// Handlebars view engine
 var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// Static folder
 app.use(express.static(__dirname + '/public'));
 
+// Port number
 const PORT = 3000;
 app.set('port', process.env.PORT || PORT);
 
-
-app.get('/', function(req, res){
+// Routes
+app.get('/', (req, res) => {
     const serverHi = greetings.getRandomGreeting();
     res.render('home', { serverHi });
 });
 
-app.get('/about', function(req, res){
+app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// custom 404 page
+app.get('/redirectme', (req, res) => {
+    res.redirect('/about');
+});
+
+// Custom 404 page
 app.use((req, res) => {
     res.status(404);
     res.render('404');
 });
 
-// custom 500 page
+// Custom 500 page
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
+// Server start
 app.listen(app.get('port'), () => {
     console.log(`Running server on port ${app.get('port')}...`);
 });
